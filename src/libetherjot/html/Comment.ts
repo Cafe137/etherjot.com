b0ffd75672e703dad8e77ba5545da12f7acd9a6a
Cafe137 <xcafe137@gmail.com>
2024-10-13 18:50:33 +0200

    chore: bump comment version

--- src/libetherjot/html/Comment.ts
+++ src/libetherjot/html/Comment.ts
@@ -1,8 +1,8 @@
 export async function createCommentSystem(commentsFeed: string) {
     return `
     <div id="comments"></div>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swarm-comment-system-ui@1.1.0/dist/style.css">
-    <script src="https://cdn.jsdelivr.net/npm/swarm-comment-system-ui@1.1.0"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swarm-comment-system-ui@1.1.1/dist/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/swarm-comment-system-ui@1.1.1"></script>
     <script>
         window.SwarmCommentSystem.renderSwarmComments('comments', { approvedFeedAddress: "${commentsFeed}" })
     </script>
